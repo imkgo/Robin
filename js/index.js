b414a9ef2c2e91542d2df964d3658788a94d75c5
孟凡凯 <mengfk@dtds.com.cn>
2016-12-01 18:19:32 +0800

    animation

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/12/1.
+ */
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
+$(document).ready(function(){
+    $(".header-img img").animateCss("bounceInDown")
+});
