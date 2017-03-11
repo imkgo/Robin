9ff7710ef5e003cd62cce709e28c6fac35bdef53
孟凡凯 <mengfk@dtds.com.cn>
2017-03-11 16:28:02 +0800

    test

--- robin.py
+++ robin.py
@@ -25,5 +25,15 @@ def hello1(user_name):
     return "<h1>Hello, %s!</h1>" % user_name
 
 
+@app.route("/<year>/<month>/<day>/<title>")
+def article(year, month, day, title):
+    print year
+    print month
+    print day
+    print title
+
+    return title
+
+
 if __name__ == '__main__':
     app.run(debug="enable")
