1198db257d99f8c4b304dd025027763e6734697a
imkgo <fankaim@sina.com>
2017-06-24 19:01:31 +0800

    详情页

--- dao/article_dao.py
+++ dao/article_dao.py
@@ -7,6 +7,7 @@ import article
 client = MongoClient('localhost', 27017)
 
 db = client['robin']
+db.authenticate("1", "1")
 
 article_col = db.article
 
@@ -16,7 +17,14 @@ def insert(art):
 
 
 def find_article_by_title(title):
-    art = article_col.find_one_and_delete({"title": title})
+    item = article_col.find_one({"title": title})
+    art = article.Article()
+    art.title = item["title"]
+    art.date = item["date"]
+    art.tags = item["tags"]
+    art.digest = item["digest"]
+    art.body = item["body"]
+    return art
 
 
 def find_all():
@@ -31,3 +39,8 @@ def find_all():
         art.body = item["body"]
         art_list.append(art)
     return art_list
+
+
+if __name__ == '__main__':
+    art = mistune_highlight.get_article("/Users/imkgo/git/github/hexo-repo/source/_posts/使用Python爬取妹子图片.md")
+    insert(art)
--- robin.py
+++ robin.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template
 import article_dao
+import logging
 app = Flask(__name__)
 
 
@@ -10,26 +11,33 @@ def hello_world():
     return render_template("index.html", articles=article_dao.find_all())
 
 
+# 根据标题获取博客
+@app.route("/<title>", methods=["post", "get"])
+def article(title):
+    print title
+    logging.info(title)
+    art = article_dao.find_article_by_title(title)
+    return render_template("article.html", article = art)
 
 
 @app.route("/ethan")
 def hello():
     return "<h1>Hello, eth11an!</h1>"
 
+#
+# @app.route("/<user_name>")
+# def hello1(user_name):
+#     return "<h1>Hello, %s!</h1>" % user_name
 
-@app.route("/<user_name>")
-def hello1(user_name):
-    return "<h1>Hello, %s!</h1>" % user_name
-
-
-@app.route("/<year>/<month>/<day>/<title>")
-def article(year, month, day, title):
-    print year
-    print month
-    print day
-    print title
-
-    return title
+#
+# @app.route("/<year>/<month>/<day>")
+# def article(year, month, day, title):
+#     print year
+#     print month
+#     print day
+#     print title
+#
+#     return title
 
 
 if __name__ == '__main__':
