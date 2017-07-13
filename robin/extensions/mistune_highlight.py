22508a8ef412854975dcc4008d0addbdc5fb660a
孟凡凯 <mengfk@dtds.com.cn>
2017-07-13 18:20:24 +0800

    代码规范化

--- robin/extensions/mistune_highlight.py
+++ robin/extensions/mistune_highlight.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # py -2 -m pip install pygments
+""" markdown文本转为html文本以及代码高亮
+"""
 import codecs
 import re
-
+from datetime import datetime
 import mistune
 from pygments import highlight
 from pygments.formatters import html
@@ -12,7 +14,8 @@ from robin.models import article
 
 
 class HighlightRenderer(mistune.Renderer):
-
+    """ 代码高亮
+    """
     def block_code(self, code, lang=None):
         if not lang:
             return '\n<pre><code>%s</code></pre>\n' % \
@@ -22,11 +25,13 @@ class HighlightRenderer(mistune.Renderer):
         return highlight(code, lexer, formatter)
 
 
-renderer = HighlightRenderer()
-markdown = mistune.Markdown(renderer=renderer)
+RENDERER = HighlightRenderer()
+MARKDOWN = mistune.Markdown(renderer=RENDERER)
 
 
 def get_article(path):
+    """ 根据文章路径获取文章对象
+    """
     art = article.Article()
     with codecs.open(path, "r", "utf-8") as mf:
         mark_txt = mf.read()
@@ -35,21 +40,24 @@ def get_article(path):
         art.body = get_body(mark_txt)
         art.tags = get_tags(mark_txt)
         art.digest = get_digest(mark_txt)
+        art.year, art.month, art.day = get_year_month_day(art.date)
     return art
 
 
 def artilce(article_str):
     art = article.Article()
-
     art.title = get_title(article_str)
     art.date = get_date(article_str)
     art.body = get_body(article_str)
     art.tags = get_tags(article_str)
     art.digest = get_digest(article_str)
+    art.year, art.month, art.day = get_year_month_day(art.date)
     return art
 
 
 def get_title(text):
+    """ 获取标题
+    """
     pattern = re.compile("title:\s(.+)[\r\n]")
     match = pattern.match(text)
     if match:
@@ -59,6 +67,8 @@ def get_title(text):
 
 
 def get_date(text):
+    """ 获取日期
+    """
     pattern = re.compile(r"date:\s(.+)[\r\n]")
     t = re.search(pattern, text)
     if t:
@@ -68,17 +78,22 @@ def get_date(text):
 
 
 def get_body(text):
+    """ 获取body
+    """
     pattern = re.compile(r"---")
-    return markdown(re.split(pattern, text)[1])
+    return MARKDOWN(re.split(pattern, text)[1])
 
 
-# 获取摘要
 def get_digest(text):
+    """ 获取摘要
+    """
     pattern = re.compile(r"---|\<\!-- more --\>")
-    return markdown(re.split(pattern, text)[1])
+    return MARKDOWN(re.split(pattern, text)[1])
 
 
 def get_tags(text):
+    """ 获取标签
+    """
     pattern = re.compile(r"tags:\s\[(.+)\]")
     rs = re.search(pattern, text)
     if rs:
@@ -87,4 +102,9 @@ def get_tags(text):
 
     return []
 
-
+def get_year_month_day(date):
+    """ 获取年份和月份
+    日期格式为：2016-12-31 14:47:07
+    """
+    article_date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+    return article_date.year, article_date.month, article_date.day
--- robin/models/article.py
+++ robin/models/article.py
@@ -1,8 +1,32 @@
-class Article(object):
+# -*- coding: utf-8 -*-
 
+""" 文章
+"""
+
+class Article(object):
+    """ 文章类
+    """
     def __init__(self):
-        pass
+        self.title = None
+        self.body = None
+        self.date = None
+        self.tags = None
+        self.digest = None
+        self.year = None
+        self.month = None
+        self.day = None
 
     @property
     def to_dic(self):
-        return {"title":self.title, "body":self.body, "date":self.date, "tags":self.tags, "digest":self.digest}
+        """ 转为字典
+        """
+        return {
+            "title":self.title,
+            "body":self.body,
+            "date":self.date,
+            "tags":self.tags,
+            "digest":self.digest,
+            "year":self.year,
+            "month":self.month,
+            "day":self.day
+            }
