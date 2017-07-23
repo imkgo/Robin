10b26641456cdb888e4f39524b7a0513d932fc10
imkgo <imkgo@imkgo-mac.local>
2017-07-23 14:33:33 +0800

    换行

--- robin/views/articles.py
+++ robin/views/articles.py
@@ -2,11 +2,10 @@
 """ 文章页面逻辑处理
 """
 
-from flask import render_template, Blueprint, request, redirect
+from flask import render_template, Blueprint
 
 from robin import MONGO
 from robin.models import article
-from robin.extensions import mistune_highlight
 
 ARTICLE_PAGE = Blueprint("article_page", __name__, template_folder="templates")
 
@@ -20,13 +19,15 @@ def index():
     for item in arts:
         art_list.append(article.article_from_dic(item))
 
-    return render_template("article_list.html", articles=art_list, github="https://github.com/imkgo")
+    return render_template("article_list.html",\
+        articles=art_list, github="https://github.com/imkgo")
 
 
 @ARTICLE_PAGE.route("/<year>/<month>/<day>/<title>", methods=["get"])
 def get_one(year, month, day, title):
     """ 根据标题获取博客
     """
-    item = MONGO.db.article.find_one({"year": int(year), "month": int(month), "day": int(day), "title": title})
+    item = MONGO.db.article.find_one({"year": int(year),\
+         "month": int(month), "day": int(day), "title": title})
     art = article.article_from_dic(item)
     return render_template("article.html", article=art)
